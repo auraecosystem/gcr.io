253813329ed1e095f38167baad423718ed9a3397
auraecosystem <sandraisom@icloud.com>
2026-06-13 01:49:57 +0100

    Enhance bash script for Docker image management
    Add commands to create a Docker repository and manage images.

--- bash.sh
+++ bash.sh
@@ -1,3 +1,14 @@
 git remote add origin https://github.com/auraecosystem/gcr.io.git
 git branch -M main
 git push -u origin main
+gcloud artifacts repositories create my-repo \
+  --repository-format=docker \
+  --location=europe \
+  --description="Docker images repo"
+  docker pull staging-image
+docker tag staging-image prod-image:v1.2.0
+docker push prod-image:v1.2.0
+
+docker pull dev-image
+docker tag dev-image staging-image
+docker push staging-image
