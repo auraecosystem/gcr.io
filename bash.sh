c56188681937bfb0daad2231284f1cb4f6214b62
auraecosystem <sandraisom@icloud.com>
2026-06-13 02:24:17 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -8,7 +8,13 @@ gcloud artifacts repositories create my-repo \
   docker pull staging-image
 docker tag staging-image prod-image:v1.2.0
 docker push prod-image:v1.2.0
+Troubleshooting info:
+  Principal: sandraisom0@gmail.com
+  Resource: web4-426302
+  Troubleshooting URL: console.cloud.google.com/iam-admin/troubleshooter/summary;permissions=resourcemanager.projects.get;token=AbColwY49yytnnLt8ki0hua1XX8oCXmaQP6olzW5OjZoJdkozv8T3HxZO97qJJHk2rYCwSVe3ZXiJmm8zyKc88joz3xb8tvrM1HrT1oq4ThPXjHIlLH8pe5SRAkLKM4_sC1okop6tq7t38K6OrikHlnsTzLzQG2jCm676-w_MYjajg?utm_campaign=error_page&utm_source=cloud_console
 
+Missing or blocked permissions:
+  resourcemanager.projects.get
 docker pull dev-image
 docker tag dev-image staging-image
 docker push staging-image
